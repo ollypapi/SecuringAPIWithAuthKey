559be6adc628f0edaccd4b981c5a07a3fa536b2e
Adebisi Adebayo Paul <ollypapi1@gmail.com>
2021-08-28 22:18:33 +0100

    first commit

---
+++ SecuringAPIWithAPIKey/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuringAPIWithAPIKey.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+        }
+    }
+}
