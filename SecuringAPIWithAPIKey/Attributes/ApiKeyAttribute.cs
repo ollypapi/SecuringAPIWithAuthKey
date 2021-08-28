62740611c21a21ca65aa83dbd65e0de4c23a0bab
Adebisi Adebayo Paul <ollypapi1@gmail.com>
2021-08-28 22:41:18 +0100

    new code

--- SecuringAPIWithAPIKey/Attributes/ApiKeyAttribute.cs
+++ SecuringAPIWithAPIKey/Attributes/ApiKeyAttribute.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,30 @@ namespace SecuringAPIWithAPIKey.Attributes
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key was not provided"
+                };
+                return;
+            }
 
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEYNAME);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key is not valid"
+                };
+                return;
+            }
+            await next();
         }
     }
 }
